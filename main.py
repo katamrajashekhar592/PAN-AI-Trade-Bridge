3e6dc50b5abb310d88da40d386111612f77c6d15
katamrajashekhar592 <katamrajashekhar592@gmail.com>
2026-06-13 21:38:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,28 +20,41 @@ def get_option_chain():
 
     url = "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY"
 
+
     headers = {
+
         "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+        "Mozilla/5.0 (Linux; Android 10)",
+
         "Accept":
         "application/json",
-        "Accept-Language":
-        "en-US,en;q=0.9",
+
+        "Accept-Encoding":
+        "gzip, deflate, br",
+
+        "Connection":
+        "keep-alive",
+
         "Referer":
         "https://www.nseindia.com/option-chain"
+
     }
+
 
     try:
 
         session = requests.Session()
 
-        session.get(
+
+        home = session.get(
             "https://www.nseindia.com",
             headers=headers,
             timeout=10
         )
 
-        time.sleep(1)
+
+        time.sleep(2)
+
 
         response = session.get(
             url,
@@ -49,12 +62,31 @@ def get_option_chain():
             timeout=10
         )
 
-        return response.json()
+
+        if response.status_code == 200:
+
+            data=response.json()
+
+            return data
+
+
+        else:
+
+            print(
+            "NSE STATUS:",
+            response.status_code
+            )
+
+            return None
+
 
 
     except Exception as e:
 
-        print("NSE ERROR:",e)
+        print(
+        "OPTION ERROR:",
+        e
+        )
 
         return None
 
