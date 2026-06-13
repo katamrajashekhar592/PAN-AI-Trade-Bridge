7d6770645b11d538ee17281684317fb0d10e7bf7
katamrajashekhar592 <katamrajashekhar592@gmail.com>
2026-06-13 20:04:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,19 +15,47 @@ def analyze():
 
     data = yf.Ticker("^NSEI")
 
-    price = data.history(period="5d")
+    df = data.history(period="1mo")
 
-    last = float(price["Close"].iloc[-1])
-    prev = float(price["Close"].iloc[-2])
-
-    change = last - prev
+    last = float(df["Close"].iloc[-1])
+    prev = float(df["Close"].iloc[-2])
 
 
-    if change > 0:
+    # EMA calculation
+    ema20 = df["Close"].ewm(span=20).mean().iloc[-1]
+    ema50 = df["Close"].ewm(span=50).mean().iloc[-1]
+
+
+    # RSI calculation
+    delta = df["Close"].diff()
+
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+
+    avg_gain = gain.mean()
+    avg_loss = loss.mean()
+
+    rsi = 100 - (100 / (1 + (avg_gain / avg_loss)))
+
+
+    score = 50
+
+
+    if last > ema20:
+        score += 20
+
+    if ema20 > ema50:
+        score += 20
+
+    if rsi < 70:
+        score += 10
+
+
+    if score >= 70:
         trend = "Bullish trend 📈"
         action = "BUY bias - momentum positive"
 
-    elif change < 0:
+    elif score <= 40:
         trend = "Bearish trend 📉"
         action = "SELL bias - weakness visible"
 
@@ -36,20 +64,25 @@ def analyze():
         action = "WAIT - no clear direction"
 
 
-    confidence = 70 if change > 0 else 40
-
-    stop_loss = last - 80
-    target = last + 120
-
-
     return {
-        "index": "NIFTY50",
-        "price": last,
-        "trend": trend,
-        "support": last - 50,
-        "resistance": last + 50,
-        "confidence": confidence,
-        "stop_loss": stop_loss,
-        "target": target,
-        "action": action
+
+        "index":"NIFTY50",
+        "price":round(last,2),
+
+        "trend":trend,
+
+        "ema20":round(ema20,2),
+        "ema50":round(ema50,2),
+
+        "rsi":round(rsi,2),
+
+        "confidence":score,
+
+        "support":round(last-50,2),
+        "resistance":round(last+50,2),
+
+        "stop_loss":round(last-80,2),
+        "target":round(last+120,2),
+
+        "action":action
     }
